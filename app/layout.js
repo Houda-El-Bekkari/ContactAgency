92a7e0e408a3398f19cd64164b9cf8e789e04acf
Houda-El-Bekkari <houda.elbekkari@etu.uae.ac.ma>
2025-11-30 17:23:56 +0100

    deletem

--- app/layout.js
+++ app/layout.js
@@ -9,13 +9,12 @@ export const metadata = {
   description: 'Plateforme de gestion des agences partenaires et de leurs contacts. Outils puissants pour optimiser votre workflow.',
   keywords: 'agences, contacts, gestion, dashboard, professionnel',
 };
-export const dynamic = 'force-dynamic';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={inter.className}>
-        <ClerkClientProvider>
+        <ClerkClientProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
           {children}
         </ClerkClientProvider>
       </body>
