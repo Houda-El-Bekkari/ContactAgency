4450a2c78818159df4e5be39eeb0af74d8a9d7b2
Houda-El-Bekkari <houda.elbekkari@etu.uae.ac.ma>
2025-11-30 18:15:58 +0100

    deletee

--- providers/clerk-provider.jsx
+++ providers/clerk-provider.jsx
@@ -5,15 +5,8 @@ import { ClerkProvider as BaseClerkProvider } from '@clerk/nextjs';
 export function ClerkClientProvider({ children }) {
   const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-  if (!publishableKey) {
-    throw new Error('Missing NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY');
-  }
-
   return (
-    <BaseClerkProvider
-      publishableKey={publishableKey}
-      localization={frFR}
-    >
+    <BaseClerkProvider publishableKey={publishableKey}>
       {children}
     </BaseClerkProvider>
   );
